89264753a566b1e6676dd77883d489b44f6582f0
Vitaliy Saburdo <vsaburdo@gmail.com>
2024-09-25 09:10:15 +0300

    create task

--- src/components/AddTaskInput/AddTaskInput.tsx
+++ src/components/AddTaskInput/AddTaskInput.tsx
@@ -1,8 +1,37 @@
-export const AddTaskInput = () => {
+import { nanoid } from 'nanoid';
+import style from './AddTaskInput.module.scss';
+
+interface Task {
+  id: string;
+  task: string;
+  status: string;
+}
+
+interface AddTaskInputProps {
+  addTask: (task: Task) => void;
+}
+
+export const AddTaskInput: React.FC<AddTaskInputProps> = ({ addTask }) => {
+  const createTodo = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const form = e.currentTarget;
+    const name = form.elements.namedItem('task') as HTMLInputElement;
+    const task = {
+      id: nanoid(6),
+      task: name.value,
+      status: 'active',
+    };
+    addTask(task);
+    form.reset();
+  };
+
   return (
     <>
-      <label htmlFor="task"></label>
-      <input name="task" type="text" />
+      <form onSubmit={createTodo} className={style.inputShell}>
+        <label htmlFor="task"></label>
+        <input className={style.input} name="task" type="text" />
+        <button type="submit" className={style.btn}></button>
+      </form>
     </>
   );
 };
--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,15 +1,28 @@
-import { AddTaskInput } from '../AddTaskInput/AddTaskInput';
+import { useState } from 'react';
+import { AddTaskInput } from '../AddTaskInput';
 import { Container } from '../Container';
 import style from './App.module.scss';
 
+interface Task {
+  id: string;
+  task: string;
+  status: string;
+}
+
 function App() {
+  const [tasks, setTasks] = useState<Task[]>([]);
+
+  const createTask = (task: Task) => {
+    setTasks([...tasks, task]);
+  };
+  console.log(tasks);
   return (
     <>
       <main className={style.main}>
         <section>
           <Container>
             <h1 className={style.title}>TODO</h1>
-            <AddTaskInput />
+            <AddTaskInput addTask={createTask} />
           </Container>
         </section>
       </main>
